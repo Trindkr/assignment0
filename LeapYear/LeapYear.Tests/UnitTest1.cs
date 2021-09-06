c80317c8143d62f6484fcd30b7af6afc1adf63b5
Trindkr <maria.trindkaer@gmail.com>
2021-09-06 15:17:14 +0200

    Added isLeapYear method + test method

--- LeapYear/LeapYear.Tests/UnitTest1.cs
+++ LeapYear/LeapYear.Tests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace LeapYear.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData(200, false)]
+        [InlineData(800, true)]
+        [InlineData(16, true)]
+        public void isLeapYear_returnsFalseOn200(int year, bool expected)
         {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            Assert.Equal(Program.isLeapYear(year),expected);
 
         }
+       
+
+
     }
 }
--- LeapYear/LeapYear/Program.cs
+++ LeapYear/LeapYear/Program.cs
@@ -1,14 +1,53 @@
 ﻿using System;
+using System.IO;
 
 namespace LeapYear
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Enter a year to check if it's a leap lear");
+            string stringInput = Console.ReadLine().Trim();
+            int input = Convert.ToInt32(stringInput);
+            /*try
+            {
+               
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Please enter a number");
+                Main(new string[0]);
+            }
+            */
+            
+            Console.WriteLine(isLeapYear(input));
+
+
         }
 
-        
+        public static bool isLeapYear(int year)
+        {
+            if(year%400==0)
+            {
+                return true;
+            }
+            else if(year%100==0)
+            {
+                return false;
+            }
+            else if(year%4==0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+
+
     }
 }
