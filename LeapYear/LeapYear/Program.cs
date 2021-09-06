fe0d6db20f32833e61484a302f666ad49ea56dae
Trindkr <maria.trindkaer@gmail.com>
2021-09-06 14:34:33 +0200

    Project setup

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        
+    }
+}
